ba2fc310076a337ee20fa94ebb3444789f8f1057
MAADA Loukmane <36376593+loding1397@users.noreply.github.com>
2019-06-28 03:20:49 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include "allegro5/allegro.h"
+#include "allegro5/allegro_image.h"
+#include "allegro5/allegro_native_dialog.h"
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_font.h>
+#include"player.h"
+#include"Obstacle.h"
+#include"Backgorund.h"
+#include"Score.h"
+#include"Window.h"
+#include"Game.h"
+
+
+using namespace std;
+
+int main(int argc, char **argv){
+
+    Game game;
+    game.run();
+    return 0;
+}
